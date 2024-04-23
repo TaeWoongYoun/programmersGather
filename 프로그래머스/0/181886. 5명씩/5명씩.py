8256aa23e057a069c058336ae451df538caec0b2
윤태웅 <107989596+TaeWoongYoun@users.noreply.github.com>
2024-04-23 16:05:51 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -1,7 +1,6 @@
 def solution(names):
     answer = []
-    list_size = 5
-    for i in range(0, len(names), list_size):
+    for i in range(0, len(names), 5):
         answer.append(names[i])
 
     return answer
