89a2a2c7252b6c3a18284545b343e4504c00efee
윤태웅 <107989596+TaeWoongYoun@users.noreply.github.com>
2024-05-27 10:52:02 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,3 +1,6 @@
 def solution(food):
-    answer = ''.join([str(i) * (food[i] // 2) for i in range(1, len(food))])
+    answer = ''
+    for i in range(1, len(food)):
+        answer += str(i) * (food[i] // 2)
+    
     return answer + '0' + answer[::-1]
