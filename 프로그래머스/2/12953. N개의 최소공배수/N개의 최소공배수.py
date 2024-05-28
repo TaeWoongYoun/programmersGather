915c2f30525967a0b3773bbad8fa58d1c1ff45fd
윤태웅 <107989596+TaeWoongYoun@users.noreply.github.com>
2024-05-28 10:02:46 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,11 +1,11 @@
 def solution(arr):
-    lcm_result = arr[0]
+    result = arr[0]
     
     for i in range(1, len(arr)):
-        a = lcm_result
+        a = result
         b = arr[i]
         while b:            
             a, b = b, a % b #유클리드 호제법
-        lcm_result = lcm_result * arr[i] // a
+        result = result * arr[i] // a
     
-    return lcm_result
+    return result
