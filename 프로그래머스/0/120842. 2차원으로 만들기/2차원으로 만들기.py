2d449003ca06d141c41a67e25e1a36ccd2a60a1f
윤태웅 <24457545yong@gmail.com>
2025-01-02 09:25:38 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,8 +1,5 @@
 def solution(num_list, n):
     answer = []
     for i in range(0, len(num_list), n):
-        line = []
-        for j in range(1):
-            line.append(num_list[i:i+n])
-        answer.append(line)
-    return sum(answer,[])
+        answer.append(num_list[i:i+n])
+    return answer
